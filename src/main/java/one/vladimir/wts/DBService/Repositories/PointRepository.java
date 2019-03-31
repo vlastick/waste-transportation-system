c7d27704d87c842a42844c86e22605ecac383083
victor40000 <victor40000@yandex.ru>
2019-03-31 16:26:54 +0300

    Added queries in Point and User repos

--- src/main/java/one/vladimir/wts/DBService/Repositories/PointRepository.java
+++ src/main/java/one/vladimir/wts/DBService/Repositories/PointRepository.java
@@ -1,7 +1,16 @@
 package one.vladimir.wts.DBService.Repositories;
 
 import one.vladimir.wts.DBService.Entities.PointEntity;
+import one.vladimir.wts.DBService.Entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PointRepository extends CrudRepository<PointEntity, Integer> {
+
+    @Query("SELECT p FROM PointEntity p where p.creator.userId = ?1")
+    List<PointEntity> findPointsByCreatorId(Integer creatorId);
+
+    List<PointEntity> findPointsByCreator(UserEntity creator);
 }
--- src/main/java/one/vladimir/wts/DBService/Repositories/UserRepository.java
+++ src/main/java/one/vladimir/wts/DBService/Repositories/UserRepository.java
@@ -1,7 +1,16 @@
 package one.vladimir.wts.DBService.Repositories;
 
 import one.vladimir.wts.DBService.Entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+
+    @Query("SELECT u.email FROM UserEntity u ")
+    List<String> findAllEmails();
+
+
 }
