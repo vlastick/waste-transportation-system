8a87cdfe3724a7ff9385c107c4c4ae92b8397f96
victor40000 <victor40000@yandex.ru>
2019-04-16 19:35:02 +0300

    [WTS-28] Added userService

---
+++ api/src/main/java/one/vladimir/api/UserService.java
@@ -0,0 +1,16 @@
+package one.vladimir.api;
+
+import one.vladimir.api.pojo.User;
+
+public interface UserService {
+
+
+    /**
+     * get current user
+     *
+     * @return user pojo
+     */
+    public User getUserId();
+
+
+}
---
+++ impl/src/main/java/one/vladimir/impl/services/user/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package one.vladimir.impl.services.user;
+
+import one.vladimir.api.Database;
+import one.vladimir.api.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service("userService")
+public class UserServiceImpl {
+    @Autowired
+    @Qualifier("database")
+    private Database db;
+
+    @PostConstruct
+    public void postConstructLog(){
+        System.out.println("userService initialized");
+    }
+
+    public User getUserId(){
+        User user = new User();
+        user.setUserId(1);
+        return user;
+    }
+
+
+}
