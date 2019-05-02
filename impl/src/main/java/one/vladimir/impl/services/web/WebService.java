33103eda82d30ccdf17787d97faa7656a6adc667
Vladimir Gromov <crashday001@yandex.ru>
2019-05-03 01:03:36 +0300

    WTS-56 Web module
    Web page for tourist was added.
    Bootstrap and css/js/jpg resources were added to the project

--- impl/src/main/java/one/vladimir/impl/services/web/WebService.java
+++ impl/src/main/java/one/vladimir/impl/services/web/WebService.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebService {
 
+    @GetMapping("/request.html")
+    public String requestPage() {
+        return "request";
+    }
+    @GetMapping("/index.html")
+    public String AboutPage() {
+        return "index";
+    }
     @GetMapping("/")
     public String touristPage() {
         return "tourist_page";
